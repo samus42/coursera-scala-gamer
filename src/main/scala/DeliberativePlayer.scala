61add92ffc07026a3aeee6e854c0eda935cf8210
Scott MacDonald <srmacdo@monsanto.com>
2015-04-20 20:33:18 -0500

    redid deliberative player

--- src/main/scala/DeliberativePlayer.scala
+++ src/main/scala/DeliberativePlayer.scala
@@ -1,15 +1,14 @@
 import org.ggp.base.player.gamer.event.GamerSelectedMoveEvent
 import org.ggp.base.player.gamer.statemachine.sample.SampleGamer
-import org.ggp.base.util.statemachine.{Move, Role, MachineState}
+import org.ggp.base.util.statemachine.{MachineState, Move, Role}
 
 import scala.collection.JavaConversions._
 
 class DeliberativePlayer extends SampleGamer {
+    val maxDepth = 10
+
     override def stateMachineSelectMove(timeout: Long) = {
         val start = System.currentTimeMillis()
-
-
-
         val selection = bestmove(getRole, getCurrentState)
         val stop = System.currentTimeMillis()
 
@@ -18,40 +17,23 @@ class DeliberativePlayer extends SampleGamer {
         selection
     }
 
-    /**
-     * function bestmove (role,state)
-      {var actions = findlegals(role,state,game);
-       var action = actions[0];
-       var score = 0;
-       for (var i=0; i<actions.length; i++)
-           {var result = maxscore(role,simulate([actions[i]],state));
-            if (result==100) {return actions[i]};
-            if (result>score) {score = result; action = actions[i]}};
-       return action}
-     */
-    private def bestmove(role: Role, state: MachineState) : Move = {
+    private def bestmove(role: Role, state: MachineState): Move = {
         val actions = getStateMachine.getLegalMoves(state, role)
-        actions.maxBy(a => maxscore(role, getStateMachine.getNextState(state, List(a))))
+        actions.maxBy(a => maxscore(role, getStateMachine.getNextState(state, List(a)), 0))
     }
 
-    /**
-     *
-     function maxscore (role,state)
-      {if (findterminalp(state,game))
-          {return findreward(role,state,game)};
-       var actions = findlegals(role,state,game);
-       var score = 0;
-       for (var i=0; i<actions.length; i++)
-           {var result = maxscore(role,simulate([actions[i]],state));
-            if (result>score) {score = result}};
-       return score}
-     */
-    private def maxscore(role: Role, state: MachineState): Int = {
-        if(getStateMachine.isTerminal(state) || getStateMachine.getGoal(state, role) == 100) {
+    private def maxscore(role: Role, state: MachineState, level: Int): Int = {
+        if (getStateMachine.isTerminal(state) || getStateMachine.getGoal(state, role) == 100) {
             getStateMachine.getGoal(state, role)
+        } else if (level >= maxDepth) {
+            simpleGoalProximity(role, state)
         } else {
             val actions = getStateMachine.getLegalMoves(state, role)
-            actions.map(m => maxscore(role, getStateMachine.getNextState(state, List(m)))).max
+            actions.map(m => maxscore(role, getStateMachine.getNextState(state, List(m)), level + 1)).max
         }
+    }
+
+    private def simpleGoalProximity(role: Role, state: MachineState) = {
+        getStateMachine.getGoal(state, role)
     }
 }
