462a9acdfeb960444b05b34b17d7fa584568e95f
Scott MacDonald <srmacdo@monsanto.com>
2015-05-02 11:16:41 -0500

    single player MCTS

--- src/main/scala/MCTSSinglePlayer.scala
+++ src/main/scala/MCTSSinglePlayer.scala
@@ -3,7 +3,7 @@ import org.ggp.base.util.statemachine.{MachineState, Move, Role}
 import scala.collection.JavaConversions._
 import scala.util.Random
 
-class MCTSPlayer extends NotifyingPlayer {
+class MCTSSinglePlayer extends NotifyingPlayer {
 
     case class GameNode(currentState: MachineState, currentPlayer: Role, move: Move = null, var visits: Int = 0, parent: GameNode = null) {
         private var cachedChildren: Option[Seq[GameNode]] = None
@@ -25,11 +25,14 @@ class MCTSPlayer extends NotifyingPlayer {
 
         def beenVisited = visits > 0
 
-        var utility: Int = 0
+        var utility: Float = 0.0f
 
-        def addVisit() = visits = visits + 1
 
-        def addUtility(score: Int) = utility += score
+        def addVisit(score: Float) = {
+            visits += 1
+            utility = utility + (score - utility) / visits
+        }
+
     }
 
     override def bestmove(role: Role, state: MachineState) = {
@@ -37,8 +40,8 @@ class MCTSPlayer extends NotifyingPlayer {
 
         val playClock = getMatch.getPlayClock * 1000
         val endTime = System.currentTimeMillis() + playClock
-        val softEnd = System.currentTimeMillis() + (playClock * .8f).toInt
-        println(s"Soft End: " + softEnd)
+        val softEnd = System.currentTimeMillis() + (playClock * .9f).toInt
+//        println(s"Soft End: " + softEnd)
         var cycles = 1
         while (System.currentTimeMillis() < softEnd) {
 //            println(s"Cycle $cycles starting.  Time Left: ${softEnd - System.currentTimeMillis()}")
@@ -49,6 +52,7 @@ class MCTSPlayer extends NotifyingPlayer {
             cycles += 1
         }
 
+        println("Utilities: " + root.children.map(_.utility).mkString(","))
         root.children.maxBy(_.utility).move
     }
 
@@ -64,13 +68,15 @@ class MCTSPlayer extends NotifyingPlayer {
     }
 
     private def selectfn(node: GameNode) = {
-        node.utility + Math.sqrt(2 * Math.log(node.parent.visits.toFloat / node.visits.toFloat))
+//        node.avgUtility + 2 * Math.sqrt(2 * Math.log(node.parent.visits.toFloat / node.visits.toFloat))
+        node.utility + Math.sqrt(2 * Math.log(node.parent.visits.toFloat) / node.visits.toFloat)
+
     }
 
     private def monteCarlo(role: Role, state: MachineState, numProbes: Int) = {
-        def depthcharge(role: Role, state: MachineState): Int = {
+        def depthcharge(role: Role, state: MachineState): Float = {
             if (isTerminal(role, state)) {
-                getStateMachine.getGoal(state, role)
+                getStateMachine.getGoal(state, role).toFloat / 100.0f
             } else {
                 val moves = getStateMachine.getRoles.map(r => {
                     val roleMoves = getStateMachine.getLegalMoves(state, r)
@@ -84,20 +90,19 @@ class MCTSPlayer extends NotifyingPlayer {
             i <- 1 to numProbes
         } yield depthcharge(role, state)
 
-        (results.sum.toFloat / numProbes.toFloat).toInt
+        results.sum / numProbes.toFloat
     }
 
     private def isTerminal(role: Role, state: MachineState) = {
-        getStateMachine.isTerminal(state) || getStateMachine.getGoal(state, role) == 100
+        getStateMachine.isTerminal(state)  || getStateMachine.getGoal(state, role) == 100
     }
 
-    private def simulate(node: GameNode): Int = {
+    private def simulate(node: GameNode): Float = {
         monteCarlo(node.currentPlayer, node.currentState, 4)
     }
 
-    private def backpropagate(node: GameNode, score: Int): Unit = {
-        node.addVisit()
-        node.addUtility(score)
+    private def backpropagate(node: GameNode, score: Float): Unit = {
+        node.addVisit(score)
         if (node.parent != null) {
             backpropagate(node.parent, score)
         }
